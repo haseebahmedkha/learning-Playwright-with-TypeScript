be2e558e89edd631edcf07d738bb4380fa85205c
haseebahmed434@gmail.com <haseebahmed434@gmail.com>
2026-05-05 00:40:00 +0500

    Add Playwright test annotations with proper usage and examples

--- tests/annotations16.spec.ts
+++ tests/annotations16.spec.ts
@@ -1,37 +1,68 @@
+import { test } from "@playwright/test";
 
+/**
+ * Topic: Playwright Test Annotations
+ * Purpose: Control test execution behavior
+ *
+ * --------------------------------------
+ * TYPES OF ANNOTATIONS
+ * --------------------------------------
+ *
+ * 1. test.only()   → run ONLY this test
+ * 2. test.skip()   → skip test execution
+ * 3. test.fail()   → expect test to fail
+ * 4. test.fixme()  → known issue, skip for now
+ * 5. test.slow()   → increases timeout for slow tests
+ */
 
-
-
-// ------------ Annotation ----------------
-//  there are 5 Types of Annotation 
-//  1. only
-//  2. skip
-//  3. fail
-//  4. fixme
-//  5. slow
-
-// we can also use in a Group Test
-
-import test from "@playwright/test";
-
-test.only("",async({})=>{
-
-});
-test.skip("",async({})=>{
-    test.skip()
-
-});
-test.fail("",async({})=>{
-
-});
-// when use when we have complete the test case it will shows in a report with skip test
-test.fixme("",async({})=>{
-
-});
-test("",async({})=>{
-    test.slow() // its will become 90 second = default time X 3 = 30 X 3 = 90 Seconds
-
+/**
+ * --------------------------------------
+ * ONLY
+ * --------------------------------------
+ * Runs only this test (used for debugging)
+ */
+test.only("Only - Debug Test", async () => {
+    console.log("This is the only running test");
 });
 
+/**
+ * --------------------------------------
+ * SKIP
+ * --------------------------------------
+ * Skips execution of this test
+ */
+test.skip("Skip - Not Ready Test", async () => {
+    console.log("This test will be skipped");
+});
 
+/**
+ * --------------------------------------
+ * FAIL
+ * --------------------------------------
+ * Marks test as expected to fail
+ */
+test.fail("Fail - Known Bug Test", async () => {
+    console.log("This test is expected to fail");
+});
 
+/**
+ * --------------------------------------
+ * FIXME
+ * --------------------------------------
+ * Used for incomplete / broken tests
+ */
+test.fixme("Fixme - Work in Progress Test", async () => {
+    console.log("This test is marked as fixme and will not run");
+});
+
+/**
+ * --------------------------------------
+ * SLOW
+ * --------------------------------------
+ * Increases timeout for this test (slow execution allowed)
+ */
+test("Slow - Heavy Test", async ({ page }) => {
+    test.slow();
+
+    console.log("This test will run with extended timeout");
+});
