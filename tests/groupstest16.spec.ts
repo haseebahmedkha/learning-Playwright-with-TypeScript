e02e691c47c03978071ac64e73691264dfd0b370
haseebahmed434@gmail.com <haseebahmed434@gmail.com>
2026-05-05 00:38:35 +0500

    Add Playwright test grouping using describe blocks with execution strategy

--- tests/groupstest16.spec.ts
+++ tests/groupstest16.spec.ts
@@ -1,32 +1,62 @@
-import {test,expect} from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
+/**
+ * Topic: Test Grouping in Playwright (test.describe)
+ * Purpose: Organize tests into logical groups for better structure and CI execution
+ */
 
-// -------- run Group of text -----------
-// npx playwright test testname.spec.ts --grep "Group Name"
-test.describe("Group One",async()=>{
-    test("test One",async({})=>{
+/**
+ * --------------------------------------
+ * RUN GROUP OF TESTS USING CLI
+ * --------------------------------------
+ *
+ * Run specific group using grep:
+ *
+ * npx playwright test --grep "Group One"
+ *
+ * This will execute only tests matching "Group One"
+ */
 
+/**
+ * --------------------------------------
+ * GROUP ONE
+ * --------------------------------------
+ */
+
+test.describe("Group One", () => {
+
+    test("Test One - Group One", async () => {
+        console.log("Executing Test One in Group One");
     });
-    test("test two",async({})=>{
 
+    test("Test Two - Group One", async () => {
+        console.log("Executing Test Two in Group One");
     });
-    test("test Three",async({})=>{
 
+    test("Test Three - Group One", async () => {
+        console.log("Executing Test Three in Group One");
     });
-    
 
-})
+});
 
-test.describe("Group Two",async()=>{
-    test("test One",async({})=>{
+/**
+ * --------------------------------------
+ * GROUP TWO
+ * --------------------------------------
+ */
 
+test.describe("Group Two", () => {
+
+    test("Test One - Group Two", async () => {
+        console.log("Executing Test One in Group Two");
     });
-    test("test two",async({})=>{
 
+    test("Test Two - Group Two", async () => {
+        console.log("Executing Test Two in Group Two");
     });
-    test("test Three",async({})=>{
 
+    test("Test Three - Group Two", async () => {
+        console.log("Executing Test Three in Group Two");
     });
-    
 
-})
+});
